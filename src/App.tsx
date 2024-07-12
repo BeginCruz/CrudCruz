cfac8cef7d7522c31a7a26d7813cd466f658540d
BeginCruz <alisson_pp81@live.com>
2024-07-12 17:32:15 -0300

    Altera02

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,25 @@ import React from 'react';
 import ProductForm from './product';
 import ListProduct from './Lista/listproduct';
 import NavBar from './components/navbar';
-import { BrowserRouter as Router, Route,Routes} from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App: React.FC = () => {
   const links = [
-    {path: '/', label: 'Inicio'},
-    {path: '/criarproduto', label:'Criar Produto'},
-    {path: '/listaproduto', label:'Lista de Produtos'},
+    { path: '/', label: 'Inicio' },
+    { path: '/criarproduto', label: 'Criar Produto' },
+    { path: '/listaproduto', label: 'Lista de Produtos' },
   ];
 
   return (
     <Router>
-      <NavBar links ={links} />
+      <NavBar links={links} />
       <Routes>
-        <Route path ="/" element ={<div>Bem vindo Pae</div>} />
+        <Route path="/" element={<div>Bem vindo Pae</div>} />
         <Route path="/criarproduto" element={<ProductForm />} />
-        <Route path="/listarprodutos" element={<ListProduct />} />
+        <Route path="/listaproduto" element={<ListProduct />} />
       </Routes>
-      </Router>
+    </Router>
   );
 };
 
-export default App
+export default App;
